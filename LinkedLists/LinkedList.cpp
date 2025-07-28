798fb27fb00ce5f6c25b9a3428a5936fea27f48f
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-07-28 00:06:47 -0700

    Add LinkedList.cpp to implement LLs - not complete

---
+++ LinkedLists/LinkedList.cpp
@@ -0,0 +1,90 @@
+//
+// Created by dev on 27/07/25.
+//
+
+class Node {
+    int data;
+    Node *next;
+    Node *prev;
+
+public:
+    // C++ makes you mark constructors that are callable with a single param to avoid implicitly assigning defaults
+    explicit Node(int data, Node *next = nullptr, Node *prev = nullptr) {
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+    }
+
+    int val() const {
+        return this->data;
+    }
+
+    Node *getNext() const {
+        return this->next;
+    }
+
+    Node *getPrev() const {
+        return this->prev;
+    }
+
+    void setVal(int val) {
+        this->data = val;
+    }
+
+    void setNext(Node *next) {
+        this->next = next;
+    }
+
+    void setPrev(Node *prev) {
+        this->prev = prev;
+    }
+};
+
+
+class LinkedList {
+    int size;
+    Node *head;
+
+public:
+    explicit LinkedList(int val) {
+        this->head = new Node(val);
+        this->size = 1;
+    }
+
+    // TODO: replace with simple size getter later on when adding insert functions that increment size
+    int getSize() const {
+        Node *current = this->head;
+        int size = 0;
+        while (current != nullptr) {
+            size++;
+            current = current->getNext();
+        }
+        return size;
+    }
+
+    Node *getHead() const {
+        return head;
+    }
+
+    bool empty() const {
+        return size == 0;
+    }
+
+    int valueAt(int index) const {
+        Node *current = this->head;
+        int i = 0;
+        while (current != nullptr) {
+            if (i == index) {
+                return current->val();
+            }
+            i++;
+            current = current->getNext();
+        }
+        return 0;
+    }
+};
+
+
+int main() {
+    LinkedList list = LinkedList(1);
+}
